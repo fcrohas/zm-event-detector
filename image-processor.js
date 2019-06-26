14d8d9aecdcbd8d97b807a6a1bd2b2896cf4e195
Fabrice Crohas <fcrohas@gmail.com>
2019-06-27 00:27:11 +0200

    Fix image to Json

--- image-processor.js
+++ image-processor.js
@@ -5,8 +5,8 @@ class ImageProcessor {
     crop(imageBuffer, box) {
         return new Promise((resolve, reject) => { 
 	   Jimp.read(imageBuffer).then((image) => {
-		image.crop(box.x, box.y, box.w, box.h);
-		image.getBase64('image/jpeg', (data) => {
+		image.crop(box.x, box.y, box.w, box.h)
+		     .getBase64(Jimp.AUTO, (err, data) => {
 		   resolve(data);  
 		});
 	   }).catch((err) => {
